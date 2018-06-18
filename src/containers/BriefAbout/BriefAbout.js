506009ae1c3c1f890dad8de14ad380cfb2c4542f
deepgunner <deepindersingh.23@gmail.com>
2018-06-18 11:04:05 +0530

    Minor changes

--- src/containers/BriefAbout/BriefAbout.js
+++ src/containers/BriefAbout/BriefAbout.js
@@ -121,8 +121,9 @@ class AboutMe extends Component {
                   <span>
                     Hi there!
                     <br/><br/>
-                    So, as you already know that my name is Deepinder Singh. I am from Delhi, India and I aspire to be a Front-End Developer.
+                    So, as you already know that my name is Deepinder Singh. I am from Faridabad, India and I aspire to be a Front-End Developer.
                   </span>
+                  
                 </LeftP>
               }
             </WaypointShow>
